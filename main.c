61ab1a16269df33bc5c389098b6b89ceedeb88d7
Soji <Soji@192.168.128.101>
2020-04-21 16:21:09 +0900

    Pressure sensor reading is debaged.

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include "i2c.h"
 #include "adc.h"
 //#include "thDrive.h"
+#include "delays.h"
 
 #pragma config FOSC = HS2	
 #pragma config PLLCFG = ON	//PLL40MHz
@@ -60,7 +61,7 @@ const unsigned long idT_BATTINFO = 22;
 
 const BYTE TH_TIMEOUT = 30;
 
-int thCmd[6];
+int thCmd[6]= {0x00,0x00,0x00,0x00,0x00,0x00};
 int thCmTest = 0;
 char thAdr[6] = {0x50,0x51,0x52,0x53,0x54,0x55};
 //char thAdr[6] = {0x29,0x2a,0x2b,0x2c,0x2d,0x2e};
@@ -69,7 +70,7 @@ int thSpd[6];
 int i = 0;
 //MS5837
 char promAdr[7] = {0xA0,0xA2,0xA4,0xA6,0xA8,0xAA,0xAC}; 
-unsigned int promCoeff[7]; 
+unsigned int promCoeff[8]; 
 signed long dT;
 signed long temp;
 signed long off;
@@ -79,6 +80,11 @@ BYTE tstMessage[4] = {0xde,0xad,0xbe,0xaf};
 unsigned int battVoltage = 0;
 unsigned short long data1 = 0;
 unsigned short long data2 = 0;
+
+int cnt=0;
+unsigned int n_prom7 = 0;
+unsigned int n_rem = 0;
+unsigned char n_bit=0;
 //????????==============================================================
 #pragma code compatible_vvector=0x08
 void compatible_interrupt(void){
@@ -96,7 +102,7 @@ void isr(void){
 		T_Counter++;
         LED_Counter++;
 		TimeOut_Counter++;
-		if(T_Counter >= 61){//10Hz
+		if(T_Counter >= 61){//10Hz //61
 			T_Counter = 0;
 			TimerFlag = TRUE;
 		}
@@ -109,14 +115,47 @@ void isr(void){
 //?????=========================================================================
 void main()
 {	
+    init();
     //InitializeESC
     for(i = 0; i < 6; i++){
         writeESCtest(thAdr[i],0x00);
     }
     //InitializeMS5837
+    
     resetMS5837();
+    Delay1KTCYx(100);
     for(i=0; i<7;i++){
         promCoeff[i] = readCalcCoeff(promAdr[i]);
+        data_T[0] = ((promCoeff[i] >> 8) & 0x00ff);
+        data_T[1] = (promCoeff[i] & 0x00ff);
+        while(!ECANSendMessage(idT_TEMP, data_T,2, ECAN_TX_STD_FRAME));
+    }
+    //int cnt;
+    promCoeff[0] = promCoeff[0] & 0x0fff;
+    //unsigned int n_prom7 = 0;
+    //unsigned int n_rem = 0;
+    //unsigned char n_bit;
+    for(i = 0; i < 8; i++){
+        promCoeff[7] = 0;
+        n_rem = 0;
+        n_bit = 0;
+        for (cnt =0; cnt < 16; cnt++){
+            if(cnt%2==1){
+                n_rem ^= (promCoeff[cnt>>1] & 0x00ff);            
+            }else{
+                n_rem ^= (promCoeff[cnt>>1] >>8);
+            }
+            for(n_bit = 8;n_bit > 0; n_bit--){
+                if(n_rem & 0x8000){
+                    n_rem = (n_rem << 1)^0x3000;
+                }else{
+                    n_rem = (n_rem <<1);
+                }
+            }
+        }
+        n_rem = (n_rem >> 12) & 0x000F;
+        data_T[0] = n_rem^0x00;
+        while(!ECANSendMessage(0xe0, data_T,1, ECAN_TX_STD_FRAME));
     }
 	while(1){		
 		if(ECANReceiveMessage(&id, data_R, &datalen_R, &flags)){
@@ -149,43 +188,40 @@ void main()
         }
 		if(TimerFlag){
 			TimerFlag = FALSE;
-            while(!ECANSendMessage(0xff, tstMessage,4, ECAN_TX_STD_FRAME));
+            //while(!ECANSendMessage(0xff, tstMessage,4, ECAN_TX_STD_FRAME));
             if(TimeOut_Counter > TH_TIMEOUT){
                 for(i =0; i<6; i++){
-                    //thCmd[i] = 0;
+                    thCmd[i] = 0;
                 }
             }
             //please write thruster controll adn getting propelloer speed method 
             //thruster write            
             for(i = 0; i < 6; i++){
-                writeESCtest(thAdr[i],thCmd[i]);
+                //writeESCtest(thAdr[i],thCmd[i]);
             }            
             //thruster read
+            
             for(i = 0; i < 6; i++){
-                thSpd[i] = readESCtest(thAdr[i]);
+                //thSpd[i] = readESCtest(thAdr[i]);
             }
             //thruster speed sending
             for(i = 0; i < 4; i++){
                 data_T[i*2] = (thSpd[i] >> 8 );
                 data_T[i*2 + 1] = (thSpd[i] & 0x00ff);
             }
-			while(!ECANSendMessage(idT_TH1, data_T,8, ECAN_TX_STD_FRAME));
+            
+			//while(!ECANSendMessage(idT_TH1, data_T,8, ECAN_TX_STD_FRAME));
             for(i = 0; i < 2; i++){
                 data_T[i*2] = (thSpd[i+4] >> 8 );
                 data_T[i*2 + 1] = (thSpd[i+4] & 0x00ff);
             }
-			while(!ECANSendMessage(idT_TH2, data_T,4, ECAN_TX_STD_FRAME));            
-            
-            //BattVoltage reading
-            battVoltage = getADC();
-            data_T[0] = (battVoltage >> 8);
-            data_T[1] = (battVoltage & 0xff);
-            while(!ECANSendMessage(idT_BATTINFO, data_T,2, ECAN_TX_STD_FRAME));
-            
+			//while(!ECANSendMessage(idT_TH2, data_T,4, ECAN_TX_STD_FRAME));  
             //MS5837 reading method
-            data1 = readMs5837ADC(0x40);
-            data2 = readMs5837ADC(0x50);
+            data1 = readMs5837ADC(0x48);
+            data2 = readMs5837ADC(0x58);
             calcValues(data1,data2);
+            temp = data2;
+            pressure = data1;
             data_T[0] = ((temp >> 24) & 0x0000ff);
             data_T[1] = ((temp >> 16) & 0x0000ff);
             data_T[2] = ((temp >> 8) & 0x0000ff);
@@ -196,6 +232,7 @@ void main()
             data_T[2] = ((pressure >> 8) & 0x0000ff);
             data_T[3] = ((pressure) & 0x0000ff);
             while(!ECANSendMessage(idT_PRES, data_T,4, ECAN_TX_STD_FRAME));
+            
 		}
 	}
 }
@@ -300,7 +337,7 @@ int readESCtest(char adress){
     return(speed);
 }
 void resetMS5837(void){
-    char adr = 0x6c;
+    char adr = 0xec;
     char cmd = 0x1e;
     IdleI2C();
     StartI2C();
@@ -321,7 +358,7 @@ void resetMS5837(void){
     
 }
 unsigned int readCalcCoeff(char memadr){
-    char adr = 0x6c;
+    char adr = 0xec;
     char cmd = memadr;
     unsigned int data;
     //unsigned char buff;
@@ -367,9 +404,9 @@ unsigned int readCalcCoeff(char memadr){
     }    
 }
 unsigned short long readMs5837ADC(char adcconf){    
-    char adr = 0x6c;
+    char adr = 0xec;
     char cmd = adcconf;
-    unsigned short long data;
+    unsigned short long data=0;
     //unsigned char buff;
     //
     IdleI2C();
@@ -390,14 +427,15 @@ unsigned short long readMs5837ADC(char adcconf){
     }
     //
     IdleI2C();
+    Delay1KTCYx(100);
     StartI2C();
     while(SSPCON2bits.SEN){}
-    if(PIR2bits.BCLIF){return(1);
+    if(PIR2bits.BCLIF){return(3);
     }else{
         if(WriteI2C(adr)){}        
         IdleI2C();
         if(!SSPCON2bits.ACKSTAT){
-            if(WriteI2C(0x00)){return(2);
+            if(WriteI2C(0x00)){return(4);
             }
         }
     }
@@ -409,7 +447,7 @@ unsigned short long readMs5837ADC(char adcconf){
     IdleI2C();
     StartI2C();
     while(SSPCON2bits.SEN){}
-    if(PIR2bits.BCLIF){return(1);
+    if(PIR2bits.BCLIF){return(5);
     }else{
         if(WriteI2C(adr | 0x01)){}        
         IdleI2C();
@@ -419,7 +457,10 @@ unsigned short long readMs5837ADC(char adcconf){
             data = ( data <<8 ) | ReadI2C();
             AckI2C();
             data = ( data <<8 ) | ReadI2C();
+            //ReadI2C();
+            //data = ( data <<8 );
             NotAckI2C();
+            //AckI2C();
             IdleI2C();
             StopI2C();
             while(SSPCON2bits.PEN){};
@@ -433,9 +474,9 @@ unsigned short long readMs5837ADC(char adcconf){
 }
 void calcValues(unsigned short long D1,unsigned short long D2){
     //first order
-    dT = D2 - promCoeff[1] * 256;
+    dT = D2 - promCoeff[5] * 256;
     temp = 2000 + dT  * promCoeff[6] / 8388608;
-    off = promCoeff[2] * 65536+ (promCoeff[2] * dT) / 128;
+    off = promCoeff[2] * 65536+ (promCoeff[4] * dT) / 128;
     sens = promCoeff[1] * 32768 + (promCoeff[3] * dT ) / 256;
     pressure = (D1 * sens / 2097152 - off) / 8192;
     //second order is not implemented 
@@ -460,9 +501,9 @@ void init(void){
     //thRev();
     OpenI2C(MASTER,SLEW_ON);
     SSPCON1bits.SSPEN = 1;
-    SSPADD = 50;
+    SSPADD = 255;
     //ADC initialize
-    OpenADC(ADC_FOSC_32 & ADC_RIGHT_JUST & ADC_8_TAD,ADC_CH0 & ADC_INT_OFF , 15);
+    //OpenADC(ADC_FOSC_32 & ADC_RIGHT_JUST & ADC_8_TAD,ADC_CH0 & ADC_INT_OFF , 15);
 }
 unsigned int getADC(void){
     ConvertADC();
